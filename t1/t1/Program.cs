0fdcd292ae17e3e4b673562dda4da93a1f180cb5
Tuomas Ojala <K2642@student.jamk.fi>
2018-09-10 18:18:56 +0300

    ok

---
+++ t1/t1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give a number > ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Number is " + number);
+            // or
+            Console.WriteLine("Number is {0}", number);
+
+        }
+    }
+}
