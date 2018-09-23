a3581e36ded564bf5dd75cc2e3f959cf7daf9b1d
Tuomas Ojala <K2642@student.jamk.fi>
2018-09-23 15:21:25 +0300

    ok

---
+++ T6/T6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matka;
+            Console.Write("Anna matka kilometreinä: ");
+            matka = int.Parse(Console.ReadLine());
+            double kulutus = matka / (100/7.02);
+            double hinta = kulutus * 1.595;
+            
+            Console.Write("Bensan kulutus on: " + kulutus + " litraa " + "ja " + hinta + " euroa");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ T9/T9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku1, luku2, luku3;
+            Console.Write("Anna ensimmäinen luku: ");
+            luku1 = int.Parse(Console.ReadLine());
+            Console.Write("Anna toinen luku: ");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.Write("Anna kolmas luku: ");
+            luku3 = int.Parse(Console.ReadLine());
+
+            if (luku1 > luku2 && luku1 > luku3)
+            {
+                Console.Write("Suurin luku on " + luku1);
+            }
+            if (luku2 > luku1 && luku2 > luku3)
+            {
+                Console.Write("Suurin luku on " + luku2);
+            }
+            if (luku3 > luku2 && luku3 > luku1)
+            {
+                Console.Write("Suurin luku on " + luku3);
+            }
+            Console.ReadLine();
+        }
+    }
+}
