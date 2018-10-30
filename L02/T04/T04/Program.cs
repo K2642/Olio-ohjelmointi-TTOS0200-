70e3bbed323bb3d68cbde4e0a79e78d4d657536b
Tuomas Ojala <K2642@student.jamk.fi>
2018-10-30 19:31:16 +0200

    ok

---
+++ L02/T04/T04/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T04
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+            int[] arvosanat = { 0, 0, 0, 0, 0, 0 };
+            int arvosana = -1;
+            int x = 1;
+
+
+            Console.WriteLine("Anna arvosanat. Syötä \"-1\" lopettaaksesi");
+
+
+            do
+            {
+                Console.WriteLine("Opiskelija " + x + ": ");
+                arvosana = int.Parse(Console.ReadLine());
+                if (arvosana > -1)
+                {
+                    arvosanat[arvosana]++;
+                }
+                x++;
+            } while (arvosana != -1);
+
+            Console.WriteLine();
+            Console.WriteLine("Arvosanajakauma:");
+            for (int i = 0; i < arvosanat.Length; i++)
+            {
+                arvosana = arvosanat[i];
+                Console.Write(i + ":");
+                while (arvosana > 0)
+                {
+                    Console.Write("*");
+                    arvosana--;
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
