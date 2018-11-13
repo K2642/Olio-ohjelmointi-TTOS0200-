4297858c1d9c1bc790078343efe2e14b66dbddc0
Tuomas Ojala <K2642@student.jamk.fi>
2018-11-13 19:00:19 +0200

    Done T01&T04

---
+++ L10/T01/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace T01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's ask user a bunch of lines!");
+            string input = "";
+            List<string> inputs = new List<string>();
+            do
+            {
+                Console.Write("Give a text line (enter ends): ");
+                input = Console.ReadLine();
+                if (input.Length > 0) inputs.Add(input);
+
+            } while (input.Length != 0);
+            Console.WriteLine("Great! Let's save them to file!");
+            System.IO.StreamWriter outputFile = null;
+            try
+            {
+                outputFile = new System.IO.StreamWriter("names.txt");
+                foreach (string line in inputs)
+                    outputFile.WriteLine(line);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Can't open file for writing (UnauthorizedAccessException)");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Opened stream is null (ArgumentNullException)");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Opened stream is not writable (ArgumentException)");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("An IO error happend (IOException)");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Some other exception happend (Exception)");
+            }
+            finally
+            {
+                // check for null because OpenWrite might have failed
+                if (outputFile != null)
+                {
+                    outputFile.Close();
+                    Console.WriteLine("Lines saved to file! Stream to file closed.");
+                }
+            }
+
+            Console.WriteLine("Let's open stream to file again and try to read those lines we wrote!");
+            Console.ReadKey();
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines("names.txt");
+                foreach (string line in lines) Console.WriteLine(line);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found (FileNotFoundException)");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ L10/T04/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace T04
+{
+    class Program
+    {
+        private static double floatnumber;
+        private static int realnumber;
+
+        static void Main(string[] args)
+        {
+            List<int> realnumbers = new List<int>();
+            List<double> floatnumbers = new List<double>();
+            string intfilename = "T2Integers.txt";
+            string doublefilename = "T2Doubles.txt";
+            string input = "";
+            Boolean isnumber = true;
+            do
+            {
+
+
+                Console.Write("Give a number (enter or not a number ends): ");
+                input = Console.ReadLine();
+
+                if (int.TryParse(input, out realnumber)) realnumbers.Add(realnumber);
+                else if (double.TryParse(input, out floatnumber)) floatnumbers.Add(floatnumber);
+                else isnumber = false;
+
+
+            } while (isnumber);
+
+            Console.WriteLine("Contents of {0}", intfilename);
+            foreach (int i in realnumbers) Console.WriteLine(i);
+            Console.WriteLine("Contents of {0}", doublefilename);
+            foreach (double d in floatnumbers) Console.WriteLine(d.ToString(".0#################"));
+            Console.WriteLine("Press any key to store in file");
+            Console.ReadKey();
+            Console.WriteLine("Great! Let's save them to file!");
+            System.IO.StreamWriter outputFile = null;
+            try
+            {
+                outputFile = new System.IO.StreamWriter(intfilename);
+                foreach (int number in realnumbers)
+                {
+                    outputFile.WriteLine(number);
+                }
+                outputFile.Close();
+                outputFile = new System.IO.StreamWriter(doublefilename);
+                foreach (double number in floatnumbers)
+                {
+                    outputFile.WriteLine(number.ToString(".0#################"));
+                }
+
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Can't open file for writing (UnauthorizedAccessException)");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Opened stream is null (ArgumentNullException)");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Opened stream is not writable (ArgumentException)");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("An IO error happend (IOException)");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Some other exception happend (Exception)");
+            }
+            finally
+            {
+                // check for null because OpenWrite might have failed
+                if (outputFile != null)
+                {
+                    outputFile.Close();
+                    Console.WriteLine("Lines saved to file! Stream to file closed.");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
